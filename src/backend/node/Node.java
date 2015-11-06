c2f190e4438bb55601eb43f74253e132b083506d
rooper <logan.rooper@gmail.com>
2015-11-06 03:17:21 -0500

    masterpiece

--- src/backend/node/Node.java
+++ src/backend/node/Node.java
@@ -1,3 +1,6 @@
+// This entire file is part of my masterpiece.
+// Logan Rooper
+
 package backend.node;
 
 import java.util.ArrayList;
@@ -6,10 +9,6 @@ import java.util.List;
 import responses.Response;
 import sharedobjects.ManipulateController;
 
-/**
- * @author loganrooper
- *
- */
 public abstract class Node implements Cloneable {
 	private String name;
 	private List<Node> myChildren;
@@ -18,70 +17,90 @@ public abstract class Node implements Cloneable {
 	public Node() {
 		myChildren = new ArrayList<Node>();
 	}
-
+	
+	/**
+	 * @param node
+	 * @return the added node
+	 */
 	public Node addChild(Node node) {
 		myChildren.add(node);
 		return node;
 	}
 
+	/**
+	 * @param nodes
+	 * @return the first added node
+	 */
 	public Node addChildren(Node... nodes) {
 		for (Node n : nodes)
 			myChildren.add(n);
 
 		return nodes[0];
 	}
-	
-	private void clearChildren() {
-		myChildren = new ArrayList<Node>();
-	}
-	
-	public Node setChildren(Node...nodes) {
-		clearChildren();
-		addChildren(nodes);
-		return nodes[0];
-	}
-	
+
+	/**
+	 * @return number of children
+	 */
 	public int getChildrenNum() {
 		return myChildren.size();
 	}
-
+	
+	/**
+	 * @param name to be set
+	 * @return this node
+	 */
 	public Node setName(String name) {
 		this.name = name;
 		return this;
 	}
 
+	/**
+	 * @return whether this node has children
+	 */
 	public Boolean hasChildren() {
 		return (myChildren.size() > 0);
 	}
 
+	/**
+	 * @return this node's children list
+	 */
 	public List<Node> getChildren() {
 		return myChildren;
 	}
 	
+	/**
+	 * @param child index
+	 * @param manipulate controller
+	 * @return the result of running the child at specified index
+	 */
 	public Response getAndRun(int index, ManipulateController mc) {
 		return myChildren.get(index).run(mc);
 	}
 	
+	/**
+	 * @param index
+	 * @return the child at specified index
+	 */
 	public Node get(int index) {
 		return myChildren.get(index);
 	}
 
 	/**
-	 * @return the myName
+	 * @return my name
 	 */
 	public String getName() {
 		return name;
 	}
 
 	/**
-	 * @return the myValue
+	 * @return my value
 	 */
 	public double getDoubleValue() {
 		return myValue;
 	}
 
 	/**
-	 * @return
+	 * @return my value as an int
 	 */
 	public int getIntegerValue() {
 		return (int) myValue;
@@ -89,8 +108,7 @@ public abstract class Node implements Cloneable {
 
 	/**
 	 * @param myValue
-	 *            the myValue to set
-	 * @return
+	 * @return sets my value
 	 */
 	public Node setValue(double myValue) {
 		this.myValue = myValue;
@@ -98,14 +116,16 @@ public abstract class Node implements Cloneable {
 	}
 
 	@Override
+	/**
+	 * @return this node
+	 */
 	public Node clone() throws CloneNotSupportedException {
 		return (Node) super.clone();
 	}
 	
 	/**
-	 * This is the main method that nodes call of each other.
-	 * @param mc
-	 * @return
+	 * @param ManipulateController
+	 * @return a response object-either success or failure
 	 */
 	public abstract Response run(ManipulateController mc);
 }
